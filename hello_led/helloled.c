fcd2202e9140f1b0f8f1ca53a364a27ca996fba5
Angelo Compagnucci <angelo.compagnucci@gmail.com>
2015-05-20 12:46:05 +0200

    kobject version

--- hello_led/helloled.c
+++ hello_led/helloled.c
@@ -8,7 +8,87 @@
 #include <linux/delay.h>
 #include <linux/interrupt.h>
 
+#define PIO_LED	40
+
+#define sysfs_dir "helloled"
+
+static struct kobject *hello_obj = NULL;
+
+static ssize_t set_value(struct device *dev,
+			struct device_attribute *attr,
+			const char *buf, size_t count) {
+
+	unsigned int value = 0;
+	int ret = 0;
+
+	ret = kstrtouint(buf, 10, &value);
+
+	switch (value) {
+		case 0:
+		case 1:
+			gpio_set_value(PIO_LED, value);
+			break;
+
+		default:
+			return -EINVAL;
+	}
+
+	return count;
+}
+
+static ssize_t show_value(struct device *dev,
+				struct device_attribute *attr,
+				char *buf) {
+	unsigned int value = 0;
+
+	value = gpio_get_value(PIO_LED);
+
+	return sprintf(buf, "%d\n", value);
+}
+
+static DEVICE_ATTR(ledcontrol, S_IWUSR | S_IRUGO,
+			show_value, set_value);
+
+static struct attribute *hello_attributes[] = {
+	&dev_attr_ledcontrol.attr,
+	NULL
+};
+
+static const struct attribute_group hello_attr_group = {
+	.attrs = hello_attributes,
+};
+
 static int __init hello_init(void) {
+	int ret = 0;
+
+	ret = gpio_request(PIO_LED,"LED");
+	if (ret != 0) {
+		pr_alert ("helloled: request gpio failed\n");
+		return ret;
+	}
+
+	ret = gpio_direction_output(PIO_LED, 1);
+	if (ret != 0) {
+		pr_alert("helloled: setting direction failed\n");
+		return ret;
+	}
+
+	hello_obj = kobject_create_and_add(sysfs_dir, kernel_kobj);
+	if (hello_obj == NULL)
+	{
+		pr_alert("helloled: kobject_create_and_add failed\n");
+		return -ENOMEM;
+	}
+
+	ret = sysfs_create_group(hello_obj, &hello_attr_group);
+	if (ret != 0)
+	{
+		/* creating files failed, thus we must remove the created directory! */
+		pr_alert ("helloled: sysfs_create_group failed with result %d\n", ret);
+		kobject_put(hello_obj);
+		return -ENOMEM;
+	}
+
 	pr_alert("helloled: Very useful LED driver up and running\n");
 
 	return 0;
@@ -16,6 +96,8 @@ static int __init hello_init(void) {
 
 static void __exit hello_exit(void)
 {
+	gpio_free(PIO_LED);
+	kobject_put(hello_obj);
 	pr_alert("helloled: Very useful LED driver down\n");
 }
 
